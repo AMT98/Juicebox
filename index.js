23dd6d388c6e0c61a634102479508d419eeb84b5
AMT98 <aswinmalla12@gmail.com>
2023-01-14 15:31:54 -0700

    create api folder with 4 files

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
+});
+
+server.use((req, res, next) => {
+  console.log("<---- Body Logger START ---->");
+  console.log(req.body);
+  console.log("<---- Body Logger END ---->");
+
+  next();
 });
